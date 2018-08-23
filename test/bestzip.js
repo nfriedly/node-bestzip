14e793e2cef18a5742c8e625f791951d454ca8af
Nathan Friedly <nathan@nfriedly.com>
2018-08-23 10:54:05 -0400

    roll archiver back to a version that supports bulk and add additional tests to exercize bulk archiving
    Fixes #19, relates to #18

--- test/bestzip.js
+++ test/bestzip.js
@@ -17,7 +17,7 @@ describe("bestzip", function() {
     });
   });
 
-  describe("When archiving a dummy file", function() {
+  describe("When archiving a file", function() {
     var destinationFile,
       file1File,
       extractFolder,
@@ -81,6 +81,107 @@ describe("bestzip", function() {
         validArchiveExtractedFile1Path = path.join(
           __dirname,
           "validArchiveExtract",
+          file1File
+        );
+
+        zip(validArchiveFilePath, [file1Path], function(zipError) {
+          expect(zipError).to.not.exist;
+
+          var unzipExtractor = unzip.Extract({
+            path: validArchiveExtractFolder
+          });
+
+          unzipExtractor.on("error", done).on("close", done);
+
+          fs.createReadStream(validArchiveFilePath).pipe(unzipExtractor);
+        });
+      });
+
+      afterEach(function(done) {
+        rimraf(validArchiveExtractFolder, function() {
+          rimraf(validArchiveFilePath, done);
+        });
+      });
+
+      it("should contain valid data after unarchive", function(done) {
+        fs.readFile(validArchiveExtractedFile1Path, function(readError, data) {
+          expect(readError).to.not.exist;
+
+          var content = data.toString();
+          expect(content).to.be.equal("1;\n");
+
+          done();
+        });
+      });
+    });
+  });
+
+  describe("When archiving a folder", function() {
+    var destinationFile,
+      file1File,
+      extractFolder,
+      fixturesFolder,
+      destinationFilePath,
+      file1Path,
+      extractFolderPath;
+
+    beforeEach(function(done) {
+      destinationFile = "fakeDestination.zip";
+      file1File = "file1.js";
+      extractFolder = "extract";
+      fixturesFolder = "fixtures";
+
+      destinationFilePath = path.join(__dirname, destinationFile);
+      file1Path = path.join(__dirname, fixturesFolder);
+      extractFolderPath = path.join(__dirname, extractFolder);
+
+      fs.mkdir(extractFolderPath, done);
+    });
+
+    afterEach(function(done) {
+      rimraf(extractFolderPath, function() {
+        rimraf(destinationFilePath, done);
+      });
+    });
+
+    it("should create archive", function(done) {
+      zip(destinationFilePath, [file1Path], function(zipError) {
+        expect(zipError).to.not.exist;
+
+        fs.stat(destinationFilePath, function(_error, stat) {
+          expect(stat).to.haveOwnProperty("birthtime");
+          done();
+        });
+      });
+    });
+
+    it("should create archive using CLI", function(done) {
+      var cliTest = new CliTest();
+      var bestzip = "node ./bin/cli.js";
+
+      // $ bestzip fakeDestination.zip fixtures/file1.js
+      cliTest.exec(`${bestzip} ${destinationFilePath} ${file1Path}`, function(
+        err,
+        res
+      ) {
+        expect(res.stdout).to.contain("zipped!");
+        done();
+      });
+    });
+
+    describe("Valid archive", function() {
+      var validArchiveFilePath,
+        validArchiveExtractFolder,
+        validArchiveExtractedFile1Path;
+
+      beforeEach(function(done) {
+        validArchiveFilePath = path.join(__dirname, "validArchive.zip");
+        validArchiveExtractFolder = path.join(__dirname, "validArchiveExtract");
+        validArchiveExtractedFile1Path = path.join(
+          __dirname,
+          "validArchiveExtract",
+          __dirname, // yes, it has this path twice. This will change in a major version, but right now we're trying to keep compatibility
+          fixturesFolder,
           file1File
         );
 
