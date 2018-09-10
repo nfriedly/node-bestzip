d5d2726e2e2d321bcfec87c1891efd1b1f825dbd
Nathan Friedly <nathan@nfriedly.com>
2018-09-10 17:39:13 -0400

    fix tests to not allow absolute paths in snapshot names

--- test/bestzip.spec.js
+++ test/bestzip.spec.js
@@ -15,21 +15,20 @@ const tmpdir = path.join(__dirname, "tmp");
 const destination = path.join(tmpdir, "test.zip");
 
 const testCases = [
-  { destination, cwd: "test/fixtures/", source: "*" }, // no .dotfiles
-  { destination, cwd: "test/fixtures/", source: "./" }, // include .dotfiles
-  { destination, cwd: "test/", source: "fixtures/*" },
-  { destination, cwd: "test/", source: "fixtures/" },
-  { destination, cwd: "test/fixtures", source: "file.txt" },
-  { destination, cwd: "test/fixtures", source: "obama.jpg" },
-  { destination, cwd: "test/fixtures", source: ["file.txt", "obama.jpg"] },
-  { destination, cwd: "test/fixtures", source: ["file.txt", ".dotfile"] },
-  { destination, cwd: "test/fixtures", source: ["file.txt", "subdir"] },
-  { destination, cwd: "test/fixtures", source: "subdir/subfile.txt" },
-  { destination, cwd: "test/", source: "fixtures/subdir/subfile.txt" },
-  { destination, cwd: "test/", source: "fixtures/*/*.txt" },
-  { destination, cwd: "test/fixtures/subdir", source: "../file.txt" }
+  { cwd: "test/fixtures/", source: "*" }, // no .dotfiles
+  { cwd: "test/fixtures/", source: "./" }, // include .dotfiles
+  { cwd: "test/", source: "fixtures/*" },
+  { cwd: "test/", source: "fixtures/" },
+  { cwd: "test/fixtures", source: "file.txt" },
+  { cwd: "test/fixtures", source: "obama.jpg" },
+  { cwd: "test/fixtures", source: ["file.txt", "obama.jpg"] },
+  { cwd: "test/fixtures", source: ["file.txt", ".dotfile"] },
+  { cwd: "test/fixtures", source: ["file.txt", "subdir"] },
+  { cwd: "test/fixtures", source: "subdir/subfile.txt" },
+  { cwd: "test/", source: "fixtures/subdir/subfile.txt" },
+  { cwd: "test/", source: "fixtures/*/*.txt" },
+  { cwd: "test/fixtures/subdir", source: "../file.txt" }
 ];
-testCases.forEach(t => (t.cwd = path.join(__dirname, "../", t.cwd))); // absolut-ize the cwd
 
 const cleanup = () =>
   new Promise((resolve, reject) => {
@@ -69,7 +68,7 @@ describe("file structure", () => {
         : testCase.source.join(" ");
 
     child_process.execSync(`node ${cli} ${destination} ${sourceArgs}`, {
-      cwd: testCase.cwd
+      cwd: path.join(__dirname, "../", testCase.cwd)
     });
 
     await unzip(destination, tmpdir);
@@ -92,13 +91,13 @@ describe("file structure", () => {
 
       child_process.execSync(
         `node ${cli} --force=node ${destination} ${sourceArgs}`,
-        { cwd: testCase.cwd }
+        { cwd: path.join(__dirname, "../", testCase.cwd) }
       );
 
       await unzip(destination, tmpdir);
       const structure = getStructure(tmpdir);
 
-      //expect(structure).toMatchSnapshot();
+      expect(structure).toMatchSnapshot();
 
       // on systems *with* a native zip, this validates that both methods output the same thing (mac, linux)
       if (testCase.structure) {
@@ -111,11 +110,16 @@ describe("file structure", () => {
   );
 
   test.each(testCases)("programmatic: %j", async testCase => {
-    await bestzip(testCase);
+    await bestzip(
+      Object.assign(
+        { destination, cwd: path.join(__dirname, "../", testCase.cwd) },
+        testCase
+      )
+    );
     await unzip(destination, tmpdir);
     const structure = getStructure(tmpdir);
 
-    //expect(structure).toMatchSnapshot();
+    expect(structure).toMatchSnapshot();
 
     if (testCase.structure) {
       expect(structure).toEqual(testCase.structure);
@@ -128,11 +132,16 @@ describe("file structure", () => {
   testIfHasNativeZip.each(testCases)(
     "programmatic with nodeZip: %j",
     async testCase => {
-      await bestzip.nodeZip(testCase);
+      await bestzip.nodeZip(
+        Object.assign(
+          { destination, cwd: path.join(__dirname, "../", testCase.cwd) },
+          testCase
+        )
+      );
       await unzip(destination, tmpdir);
       const structure = getStructure(tmpdir);
 
-      //expect(structure).toMatchSnapshot();
+      expect(structure).toMatchSnapshot();
 
       // on systems *with* a native zip, this validates that both methods output the same thing (mac, linux)
       if (testCase.structure) {
