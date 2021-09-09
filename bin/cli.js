01a4081273f7b05caedcc8a151f8def45693bb1e
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2021-09-09 20:24:19 +0200

    Add option to set level of compression (#52)
    * Add option to set level of compression
    
    * Fix default compression level
    
    Co-authored-by: Patrik Valkovic <patrik.valkovic@qest.cz>

--- bin/cli.js
+++ bin/cli.js
@@ -10,10 +10,20 @@ var argv = require("yargs")
     describe: "Force use of node.js or native zip methods",
     choices: ["node", "native"],
   })
+  .option("level", {
+    describe: "Level of compression",
+    type: "number",
+    default: -1,
+  })
   .demand(2).argv;
 
 var destination = argv._.shift();
 var source = argv._;
+
+if (argv.level < -1 || argv.level > 9) {
+  console.error("Invalid compression level, must be >= 0 and <= 9");
+  process.exit(1);
+}
 
 console.log("Writing %s to %s...", source.join(", "), destination);
 
@@ -26,7 +36,8 @@ if (argv.force === "node") {
 zip({
   source: source,
   destination: destination,
-  verbose: argv.verbose,
+  verbose: !!argv.verbose,
+  level: argv.level,
 })
   .then(function () {
     console.log("zipped!");
